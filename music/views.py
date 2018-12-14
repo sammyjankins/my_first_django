4c72aa5b571385f5737eb44828b71cbb258158fe
sammyjankins <neroragno@mail.ru>
2018-12-14 18:20:21 +0700

    Using templates in views

--- music/views.py
+++ music/views.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Album
+
 
 def index(request):
     all_albums = Album.objects.all()
-    html = ''
-    for album in all_albums:
-        url = '/music/{}/'.format(album.id)
-        html += '<a href="{}">{}</a><br>'.format(url, album.album_title)
-    return HttpResponse(html)
+    context = {'all_albums': all_albums}
+    return render(request, 'music/music.html', context)
 
 
 def detail(request, album_id):
-    return HttpResponse('<h2>Details for album {}: </h2>'.format(album_id))
+    context = {'album_id': album_id}
+    return render(request, 'music/detail.html', context)
