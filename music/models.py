fbcaf8011b879db4f8c1e7dab2d57e9c57129f6b
sammyjankins <neroragno@mail.ru>
2018-12-14 15:01:39 +0700

    App was added to setting Str rep was added to album

--- music/models.py
+++ music/models.py
@@ -10,9 +10,11 @@ class Album(models.Model):
     genre = models.CharField(max_length=100)
     album_logo = models.CharField(max_length=1000)
 
+    def __str__(self):
+        return '{} - {} ({})'.format(self.artist, self.album_title, self.genre)
+
 
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
-    song_title =  models.CharField(max_length=250)
-
+    song_title = models.CharField(max_length=250)
