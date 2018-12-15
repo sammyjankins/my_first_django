28512accf625e11e5b7e6061e11844d37e54ef58
sammyjankins <neroragno@mail.ru>
2018-12-15 23:15:31 +0700

    Using generic in views

--- music/urls.py
+++ music/urls.py
@@ -5,12 +5,9 @@ app_name = 'music'
 
 urlpatterns = [
 
-    #/music/
-    url(r'^$', views.index, name='index'),
+    # /music/
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
     # /music/album_id/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
-
-    # /music/album_id/favorite
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 ]
