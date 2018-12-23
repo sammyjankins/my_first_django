31be716a4255ff1f81f6ecd8fad2f25031a76057
sammyjankins <neroragno@mail.ru>
2018-12-23 14:09:38 +0700

    album form + saving album logo

--- music/models.py
+++ music/models.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
-from  django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse
+from urllib import request
 
 
 class Album(models.Model):
@@ -10,6 +11,21 @@ class Album(models.Model):
     genre = models.CharField(max_length=100)
     album_logo = models.CharField(max_length=1000)
     file_type = models.CharField(max_length=10)
+
+    def __init__(self, *args, **kwargs):
+        models.Model.__init__(self, *args, **kwargs)
+        if self.album_logo[:4] == 'http':
+            self.store_logo()
+
+    def store_logo(self):
+        resource = request.urlopen(self.album_logo)
+        out = open('/home/sammyjankins/PycharmProjects/my_first_django/music/static/music/images/img{}.jpg'.
+                   format(self.pk), 'wb')
+        out.write(resource.read())
+        out.close()
+        self.album_logo = "../../static/music/images/img{}.jpg".\
+            format(self.pk)
+        self.save()
 
     def get_absolute_url(self):
         return reverse('music:detail', kwargs={'pk': self.pk})
